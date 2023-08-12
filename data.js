c2574b5db9570e2c83a8f875c59712e7c70ef93b
KavyaCOder1 <97831532+KavyaCOder1@users.noreply.github.com>
2023-08-12 12:01:08 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,41 @@
+module.exports = {
+
+    mongoLink:'mongodb+srv://manishprajapati7517:R4gO0q3MFPDbjqKp@kavyatg.zgsh5nz.mongodb.net/?retryWrites=true&w=majority', // mongo-link from cloud.mongodb.com
+    
+    bot_token:'6207203256:AAFK-wbW-5d0VUt_9Qt-_ymz-BVdbg7b0LE',//bot token
+    
+    bot_name:'DGB_Autopay_V1bot',//bot username without @
+    
+    bot_admin:5556717138,// admin telegram id
+    
+    channelsList : ['@AirdropAcid' , '@airdropdetectivesss' , '@airdropadvertisers1' , '@PerfectAirdrop_Z' , '@workandearnfrom_home' , '@asianairdropz', '@onlineMoney_Earning'],// channels with check
+
+    channel123: ['@AirdropAcid' , '@airdropdetectivesss' , '@airdropadvertisers1' , '@PerfectAirdrop_Z' , '@workandearnfrom_home' ,  '@onlineMoney_Earning' , '@uxuyprotocolWW'   ] , // channel to display in bot don't enter payment channel
+
+    twitter:'https://youtube.com/@WorkandEarnFromHome',//your twitter link
+
+    task1channel : ['@PayoutHub9109'], // task 1 channels
+
+    reffer_bonus:0.15,// refferal bonus amount
+    
+    min_wd:0.5,//min withdrawal amount
+    
+    daily_bonus:0.0005,// daily bonus amount
+
+    instantfundadd: 1,//FOR WITHDRAWAL TEST
+
+    task_bonus1: 0.0005,// task 1 bonus amount
+
+    task_bonus2: 0.0005,//task 2 bonus amount
+
+    privateKey: 'L1B3TETNiSyHqdQM8WMhxTutnwYMCYUgfEVDjQ8tHKwbujCXhwv5', //private key of wallet
+
+    address: 'D5ip1SBMk1vE74U9V5vkHU61ZxRy87XStF',//Celo  address of wallet
+    
+    currency:'DGB', //bot currency
+    
+    payment_channel:'@asianairdropz',// payment channel with @
+
+    payment_channel1: 'asianairdropz'// enter payment channel without @ 
+    
+    }
